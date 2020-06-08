2269219a4f0f27ea454fd4d79f1267878ee149a7
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-06-08 20:34:59 +0700

    add carousel_slider dependency and register assets

--- lib/ui/mainpage/dashboard.dart
+++ lib/ui/mainpage/dashboard.dart
@@ -11,8 +11,47 @@ class DashboardScreen extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("This is Dashboard Screen"),
+      body: Container(
+        margin: EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Related News",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "View All",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(width: 5,),
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Color(0xff00C4FF),
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                      ),
+                      child: Image.asset(
+                        "assets/icon-angle-right.png",
+                        width: 10,
+                        height: 20,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
