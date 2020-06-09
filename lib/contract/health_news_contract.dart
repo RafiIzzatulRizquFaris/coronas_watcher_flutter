12b2101369911b7954af3e5eb6daf48c70322cb7
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-06-09 08:39:36 +0700

    Create health_news_contract.dart

---
+++ lib/contract/health_news_contract.dart
@@ -0,0 +1,10 @@
+import 'package:coronaswatcherflutter/model/health_news_model.dart';
+
+class HealthNewsContractPresenter{
+  getHealthNews() async {}
+  loadData(){}
+}
+
+class HealthNewsContractView{
+  setHealthNews(HealthNewsModel newsModel) async {}
+}
