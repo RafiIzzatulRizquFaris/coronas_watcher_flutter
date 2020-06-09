2e58c4e466df6275a58d9bdf5eb18dda45fea87a
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-06-09 08:41:53 +0700

    completing carousel for related news section

--- lib/ui/mainpage/dashboard.dart
+++ lib/ui/mainpage/dashboard.dart
@@ -1,3 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:coronaswatcherflutter/contract/health_news_contract.dart';
+import 'package:coronaswatcherflutter/model/health_news_model.dart';
+import 'package:coronaswatcherflutter/presenter/health_news_presenter.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatefulWidget {
@@ -7,52 +11,155 @@ class Dashboard extends StatefulWidget {
   }
 }
 
-class DashboardScreen extends State<Dashboard> {
+class DashboardScreen extends State<Dashboard>
+    implements HealthNewsContractView {
+  List<Article> _listHealthArticle = List<Article>();
+  HealthNewsPresenter _healthNewsPresenter;
+  var isLoading;
+
+  DashboardScreen() {
+    _healthNewsPresenter = HealthNewsPresenter(this);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isLoading = true;
+    _healthNewsPresenter.loadData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.all(10),
         child: ListView(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Related News",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "View All",
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                    SizedBox(width: 5,),
-                    Container(
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: Color(0xff00C4FF),
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.all(Radius.circular(5)),
-                      ),
-                      child: Image.asset(
-                        "assets/icon-angle-right.png",
-                        width: 10,
-                        height: 20,
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
+            relatedNewsTitle(),
+            isLoading ? FlutterLogo() : relatedNewsCarousel(),
           ],
         ),
       ),
     );
+  }
+
+  relatedNewsTitle() {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10, top: 10,),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Related News",
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          Row(
+            children: [
+              Text(
+                "View All",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Container(
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Color(0xff00C4FF),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                ),
+                child: Image.asset(
+                  "assets/icon-angle-right.png",
+                  width: 7.5,
+                  height: 15,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  relatedNewsCarousel() {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10, top: 5,),
+      child: CarouselSlider.builder(
+        itemCount: _listHealthArticle.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: EdgeInsets.all(5),
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              child: Stack(
+                children: [
+                  Image.network(
+                    _listHealthArticle[index].urlToImage,
+                    fit: BoxFit.cover,
+                    width: 1000,
+                  ),
+//                Container(
+//                  decoration: BoxDecoration(
+//                    color: Colors.grey,
+//                    shape: BoxShape.rectangle,
+//                  ),
+//                ),
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromARGB(200, 0, 0, 0),
+                            Color.fromARGB(0, 0, 0, 0),
+                          ],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 10,
+                      ),
+                      child: Text(
+                        _listHealthArticle[index].title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        options: CarouselOptions(
+          aspectRatio: 2,
+          enlargeCenterPage: true,
+          scrollDirection: Axis.horizontal,
+          autoPlay: true,
+        ),
+      ),
+    );
+  }
+
+  @override
+  setHealthNews(HealthNewsModel newsModel) async {
+    if (newsModel.article.isNotEmpty) {
+      setState(() {
+        _listHealthArticle = newsModel.article;
+        isLoading = false;
+      });
+    } else {
+      print("Object is empty");
+    }
   }
 }
