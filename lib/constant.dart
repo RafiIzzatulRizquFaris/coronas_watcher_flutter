621f5441a94f7c25b80ea775875abbb9ae6afed8
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-06-09 08:38:49 +0700

    create constant.dart for Constants variable

---
+++ lib/constant.dart
@@ -0,0 +1,5 @@
+class Constant{
+  static const String NEWS_API_KEY = "dfe1755802a84ebc9e787d53a45d56df";
+  static const String HEALTH_NEWS_CATEGORY = "health";
+  static const String NEWS_API_URL = "https://newsapi.org/v2/";
+}
