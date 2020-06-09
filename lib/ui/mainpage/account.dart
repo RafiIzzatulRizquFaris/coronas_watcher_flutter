66fff7c99c5c731fbfd42cc87d160eb12df9f69d
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-06-09 21:09:18 +0700

    create account section

--- lib/ui/mainpage/account.dart
+++ lib/ui/mainpage/account.dart
@@ -11,9 +11,81 @@ class AccountScreen extends State<Account> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("This is Account Screen"),
+      body: Container(
+        margin: EdgeInsets.all(15),
+        child: ListView(
+          children: [
+            accountSection(),
+//            profileSection(),
+          ],
+        ),
       ),
     );
   }
+
+  accountSection() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Account",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.only(
+              left: 5,
+              top: 10,
+            ),
+            child: Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey,
+                  ),
+                  width: 40,
+                  height: 40,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Max Verstappen",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          Text(
+                            "your cRaSh",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Image.asset(
+                        "assets/icon-angle-right.png",
+                        color: Colors.black,
+                        width: 20,
+                        height: 30,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
 }
