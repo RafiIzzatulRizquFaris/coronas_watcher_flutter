c222c4bc4d02853d068b9b35db468ac2bba0f302
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-06-11 07:59:22 +0700

    create world_total_contract.dart

---
+++ lib/contract/world_total_contract.dart
@@ -0,0 +1,10 @@
+import 'package:coronaswatcherflutter/model/world_total_model.dart';
+
+class WorldTotalContractPresenter{
+  getWorldTotal()async{}
+  loadData(){}
+}
+
+class WorldTotalContractView{
+  setWorldTotal(WorldTotalModel worldTotalModel) async {}
+}
